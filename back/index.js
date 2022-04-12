bcc04955a95560bec25759ff3c8c116cbbf769c7
dodadoa <maddodadoa@gmail.com>
2022-04-12 21:01:21 +0700

    feature: connect osc and websocket from back to front

--- back/index.js
+++ back/index.js
@@ -1,10 +1,10 @@
-import { Server } from 'node-osc';
-import { WebSocketServer } from 'ws';
+import { Server } from 'node-osc'
+import { WebSocketServer } from 'ws'
+import osc from 'osc'
 
 const WEBSOCKET_PORT = process.env.WEBSOCKET_PORT || 8080
 const OSC_SERVER_HOST = process.env.OSS_SERVER_HOST || '127.0.0.1'
 const OSC_SERVER_PORT = process.env.OSC_SERVER_PORT || 3333
-
 
 const wss = new WebSocketServer({ port: WEBSOCKET_PORT });
 const oscServer = new Server(OSC_SERVER_PORT, OSC_SERVER_HOST, () => {
@@ -12,17 +12,29 @@ const oscServer = new Server(OSC_SERVER_PORT, OSC_SERVER_HOST, () => {
 });
 
 wss.on('connection', function connection(ws) {
+  ws.binaryType = 'arraybuffer'
+
   console.log(`Websocket Server is listening on port:${WEBSOCKET_PORT}`)
-  ws.send('Connected')
+  const connected = {
+    address: '/connected',
+    args: ['Connected']
+  }
+  ws.send(osc.writePacket(connected))
 
   ws.on('message', function message(data) {
     console.log('received: %s', data);
   });
 
   oscServer.on('message', function (msg, sender) {
-    console.log(`Message: ${msg}. FROM: ${sender.address}:${sender.port}`);
-    ws.send(`${msg}`);
+    const [address, message] = `${msg}`.split(",")
+    const encoded = osc.writePacket({
+      address,
+      args: [
+        message
+      ]
+    })
+    console.log(`Message: ${msg} -> ${encoded}. FROM: ${sender.address}:${sender.port}`);
+    ws.send(encoded);
   });
-
 });
 
--- front/src/App.js
+++ front/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react'
+import './App.css'
+import osc from 'osc/dist/osc-browser.min.js'
 
 function App() {
+
+  useEffect(() => {
+    const oscPort = new osc.WebSocketPort({
+      url: "ws://localhost:8080",
+      metadata: true
+    });
+    oscPort.open()
+    oscPort.on("message", function (oscMsg) {
+      console.log(oscMsg)
+      console.log("An OSC message just arrived!");
+    });
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
