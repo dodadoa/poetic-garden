136b83cdc5d8bfe8ea640fa86bc0454fff039365
dodadoa <maddodadoa@gmail.com>
2022-04-12 14:26:02 +0700

    receiving osc and sending websocket

--- back/index.js
+++ back/index.js
@@ -1,10 +1,24 @@
 import { Server } from 'node-osc';
+import { WebSocketServer } from 'ws';
 
-var oscServer = new Server(3333, '0.0.0.0', () => {
+const wss = new WebSocketServer({ port: 8080 });
+const oscServer = new Server(3333, '0.0.0.0', () => {
   console.log('OSC Server is listening');
 });
 
-oscServer.on('message', function (msg) {
-  console.log(`Message: ${msg}`);
-  oscServer.close();
+wss.on('connection', function connection(ws) {
+  console.log('Websocket Server is listening')
+
+  ws.on('message', function message(data) {
+    console.log('received: %s', data);
+  });
+
+  oscServer.on('message', function (msg) {
+    console.log(`Message: ${msg}`);
+    ws.send(`Message: ${msg}`);
+    oscServer.close();
+  });
+
+  ws.send('something');
 });
+
