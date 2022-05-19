95bab9e0919006420e9df865be9d15b1405044c8
dodadoa <maddodadoa@gmail.com>
2022-05-19 18:44:07 +0700

    remove hardcode ip and use env instead

--- tool/osc-client.js
+++ tool/osc-client.js
@@ -1,6 +1,6 @@
 import { Client } from 'node-osc';
 
-const client = new Client('172.22.106.111', 3333)
+const client = new Client(process.env.CLIENT_IP, 3333)
 client.send('/oscMessage', Math.random(), () => {
   client.close()
 })
